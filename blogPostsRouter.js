453c229bed907786f76dc0030555bf9812dc91b1
ljyockey <linyockey@gmail.com>
2017-04-23 21:34:33 -0700

    added create functions

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -6,4 +6,8 @@ const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models');
 
+BlogPosts.create('blog-1', 'Look! It\'s my first blog!', 'LJ' );
+BlogPosts.create('blog-2', 'Oh boy! What a wonderful day!', 'Mickey Mouse');
+BlogPosts.create('blog-3', 'It\'s lev-i-O-sa!', 'Hermione');
+
 module.exports = router;
