03df70bc6c3bddb2c1270d5f31da3fa5f2978ec8
ljyockey <linyockey@gmail.com>
2017-04-27 21:21:10 -0700

    added DELETE test

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -48,4 +48,15 @@ describe('Blog Posts', function() {
 			res.body.should.deep.equal(Object.assign(updateData, {publishDate: res.body.publishDate}));
 		});
 	});
+	it('should remove post on DELETE', function() {
+		return chai.request(app)
+		.get('/blog-posts')
+		.then(function(res) {
+			return chai.request(app)
+			.delete(`/blog-posts/${res.body[0].id}`);
+		})
+		.then(function(res) {
+			res.should.have.status(204);
+		});
+	});
 });
